2ade2a269ae8079dc7ef49f86c46ca26986c6d73
belkud <vlad-belkud@yandex.ru>
2023-12-23 12:41:44 +0300

    Startg

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,22 @@ import './style.css'
 
 
 const home = document.querySelector ('#home') as HTMLDivElement
-// home.addEventListener ('click', ()=> {
+const app = document.querySelector ('#app') as HTMLDivElement
   setInterval(() => {
     if (home.style.color =='orange') {
       home.style.color= 'greenyellow'
+      app.innerHTML = `greenyellow`
+      app.style.color= 'greenyellow'
     } else {
       home.style.color = 'orange'
+      app.innerHTML = `orange`
+      app.style.color = 'orange'
     }
     
   }, 1000);
    
   
-// })
+
+console.log(home.style.color);
+
 
