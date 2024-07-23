ef6c2d811fee6b202104ffe7cc71745ebfdc1096
belkud <vlad-belkud@yandex.ru>
2024-07-23 20:02:43 +0300

    fg

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,6 @@ import './style.css'
 
 
 
-
-
-
-
 // await fetch('https://jsonplaceholder.typicode.com/photos')
 
 
@@ -59,63 +55,63 @@ console.log(formData1.get('name'))
 
 
 
-const url = fetch('https://api.github.com/users/belkud')
+// const url = fetch('https://api.github.com/users/belkud')
 
 
-async function gitPhoto () {
-    try {
-        const response = await url
+// async function gitPhoto () {
+//     try {
+//         const response = await url
   
-        //! Проверка статуса сервера
-        if(response.ok) {
-            setTimeout(()=> {
-                persons.innerHTML += `<div id="request">Запрос выполнен</div>
-                <img id="avatar" src="https://avatars.githubusercontent.com/u/126806058?v=4" alt="">`
-            },0) 
-            // setTimeout(() => {
-            //    persons.innerHTML = `<div id="request">Запрос выполнен</div>` 
-            // }, 6000);
-        } else {
-            persons.innerHTML += 'Ошибка сервера <br>'     
-        }
-        const photo = await response.blob()
-        console.log(response.ok);
-        console.log(response.headers.get('Content-length'));
-        console.log(response.headers.get('Content-type'));
+//         //! Проверка статуса сервера
+//         if(response.ok) {
+//             setTimeout(()=> {
+//                 persons.innerHTML += `<div id="request">Запрос выполнен</div>
+//                 <img id="avatar" src="https://avatars.githubusercontent.com/u/126806058?v=4" alt="">`
+//             },0) 
+//             // setTimeout(() => {
+//             //    persons.innerHTML = `<div id="request">Запрос выполнен</div>` 
+//             // }, 6000);
+//         } else {
+//             persons.innerHTML += 'Ошибка сервера <br>'     
+//         }
+//         const photo = await response.blob()
+//         console.log(response.ok);
+//         console.log(response.headers.get('Content-length'));
+//         console.log(response.headers.get('Content-type'));
         
-        console.log(photo);
+//         console.log(photo);
         
-    } catch (error:any) {
-        persons.innerHTML += `ошибка: ${error.message}`
-    }
-}
+//     } catch (error:any) {
+//         persons.innerHTML += `ошибка: ${error.message}`
+//     }
+// }
 
-gitPhoto()
+// gitPhoto()
 
-const check = document.querySelector('#check') as HTMLDivElement
-check.insertAdjacentHTML('afterbegin', '<p>afterbegin</p>')
+// const check = document.querySelector('#check') as HTMLDivElement
+// check.insertAdjacentHTML('afterbegin', '<p>afterbegin</p>')
 
-const pig = document.querySelector('#pig') as HTMLDivElement
-pig.style.backgroundColor = 'aqua'
-pig.style.border = '2px solid red'
+// const pig = document.querySelector('#pig') as HTMLDivElement
+// pig.style.backgroundColor = 'aqua'
+// pig.style.border = '2px solid red'
 
 //! два способа написания
-pig.style.height = '250px'
-// pig.style.height = 250 + 'px'
+// pig.style.height = '250px'
+// // pig.style.height = 250 + 'px'
 
-pig.insertAdjacentHTML('afterbegin', '<p>afterbegin</p>')
-pig.insertAdjacentHTML('beforebegin', '<p>beforebegin</p>')
-pig.insertAdjacentHTML('afterend', '<p>afterend</p>')
-pig.insertAdjacentHTML('beforeend', '<p>beforeend</p>')
+// pig.insertAdjacentHTML('afterbegin', '<p>afterbegin</p>')
+// pig.insertAdjacentHTML('beforebegin', '<p>beforebegin</p>')
+// pig.insertAdjacentHTML('afterend', '<p>afterend</p>')
+// pig.insertAdjacentHTML('beforeend', '<p>beforeend</p>')
 
-pig.innerHTML +='<img src="public/vite.svg">'
+// pig.innerHTML +='<img src="public/vite.svg">'
 
 
 
-let divStart = document.createElement('div') as HTMLDivElement
-divStart.innerHTML = 'DIV создан в ts prepend'
-divStart.style.backgroundColor = 'pink'
-divStart.style.color = 'aliceblue'
+// let divStart = document.createElement('div') as HTMLDivElement
+// divStart.innerHTML = 'DIV создан в ts prepend'
+// divStart.style.backgroundColor = 'pink'
+// divStart.style.color = 'aliceblue'
 
 // <img src=https://ir.ozone.ru/s3/multimedia-1/wc500/6847439437.jpg>
 
@@ -124,20 +120,17 @@ divStart.style.color = 'aliceblue'
 
 
 
-let divEnd = document.createElement('div') 
-divEnd.innerHTML = 'создан в ts, метод prepand'
-divEnd.style.color = 'white'
-divEnd.style.backgroundColor = 'yellowgreen'
-pig.append(divEnd)
+// let divEnd = document.createElement('div') 
+// divEnd.innerHTML = 'создан в ts, метод prepand'
+// divEnd.style.color = 'white'
+// divEnd.style.backgroundColor = 'yellowgreen'
+// pig.append(divEnd)
 
 
-const url2 = fetch('https://api.github.com/users/belkud')
-.then(mylog=>mylog.json())
-.then(login =>console.log(login.login))
-console.log(url2);
-
-
-
+// const url2 = fetch('https://api.github.com/users/belkud')
+// .then(mylog=>mylog.json())
+// .then(login =>console.log(login.login))
+// console.log(url2);
 
 
 
@@ -149,17 +142,18 @@ console.log(url2);
 
 
 
-async function learnJS () {
-    const response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
-    const json = await response.json()
-    // console.log(json);
+
+
+
+// async function learnJS () {
+//     const response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+//     const json = await response.json()
+//     // console.log(json);
     
     
-}
+// }
 
-learnJS()
-
-
+// learnJS()
 
 
 
@@ -174,40 +168,42 @@ learnJS()
 
 
 
-async function users() {
-    try {
+
+
+// async function users() {
+//     try {
         
-        const resp = await fetch('https://jsonplaceholder.typicode.com/users', {
-                method:'POST',
-                headers: {
-                        "Content-type":"application/json"
-                    },
-                    body:JSON.stringify({
-                            name: "Mary",
-                        formData: {
-                            "Content-Type": "multipart/form-data"
-                        }
-                    })
-                })
+//         const resp = await fetch('https://jsonplaceholder.typicode.com/users', {
+//                 method:'POST',
+//                 headers: {
+//                         "Content-type":"application/json"
+//                     },
+//                     body:JSON.stringify({
+//                             name: "Mary",
+//                         formData: {
+//                             "Content-Type": "multipart/form-data"
+//                         }
+//                     })
+//                 })
                 
-        const json = await resp.json()
-        console.log(json);
+//         const json = await resp.json()
+//         console.log(json);
         
                 
-                //! splice - выводит по id
-                // console.log(json.splice(5,10));
-                let key
-                for (key in json) {
-                    json[key]
-                    // console.log(json[key]);
-                } 
-            } catch (error) {
-                console.log(error);
+//                 //! splice - выводит по id
+//                 // console.log(json.splice(5,10));
+//                 let key
+//                 for (key in json) {
+//                     json[key]
+//                     // console.log(json[key]);
+//                 } 
+//             } catch (error) {
+//                 console.log(error);
                 
-            }
-        }
+//             }
+//         }
     
-        users()
+//         users()
 
     
         // async function loadJson(url) {
@@ -316,10 +312,10 @@ async function users() {
 const stars = document.querySelector('#star') as HTMLDivElement
 
 stars.addEventListener('click', (event)=> {
-    const target = event.target as HTMLDivElement
+    const pressStar = event.target as HTMLDivElement
     
-    if (target.dataset.set) {
-        let x = +target.dataset.set 
+    if (pressStar.dataset.set) {
+        let x = +pressStar.dataset.set 
         for (let i of stars.children) {
             const el = i as HTMLDivElement
          if (el.dataset.set && +el.dataset.set<=x) {
@@ -331,4 +327,394 @@ stars.addEventListener('click', (event)=> {
     }
     
 })
+
+ console.log('___________');
+ 
+ 
+ 
+ const text = document.querySelector('#check2') as HTMLDivElement
+ text.addEventListener('click', ()=> {
+    
+        text.style.color='red'
+    text.innerHTML += 'k'
+    })
+    text.style.cursor = 'POINTER'
+    console.log(text.style.cursor);
+    
+
+
+// document.body.innerHTML += 'adsfsdfsdfsdf'
+
+
+    
+ 
+ console.log('hi');
+ console.dir('hi')
+ console.table('hi')
+ 
+ const anim = 'pig'
+ console.log(anim);
+ 
+ const animMass = {
+    color:'pink',
+    age:3
+ }
+console.table(animMass);
+
+const digit = [3, 5, 7]
+
+console.log(digit.map(el=>el*2));
+
+
+
+// округлять к ближайшему нечетному числу
+
+const x = 5.3
+console.log(2*Math.ceil(x/2));
+
+
+
+const oddNumber = [2, 3, 6, 8, 5, 0] as any
+oddNumber.forEach(el => {
+ if (el%2!=0) {
+     console.log(el);
+ }   
+}) 
+
+let acc = 0
+oddNumber.forEach (el=> {
+    acc += el
+})
+console.log(acc);
+
+
+
+const odd = (el) => el%2!=0
+console.log(oddNumber.filter(odd));
+
+
+let acc2 = 0
+for (let i = 0; i < oddNumber.length; i++) {
+    acc2+=oddNumber[i]    
+}
+console.log('acc2 = ' + acc2);
+
+
+let letter = [ 'a', 'b', 'a', 'e', 'a', 'e']
+
+let letterAcc = 0 as any
+letter.forEach(el=> {
+    if (el == 'a') {
+        letterAcc += 1
+    } 
+})
+
+console.log(letterAcc);
+
+
+// if (el=='a' || el=='b' || el =='e') {
+//     // if (el == 'q') {
+//         letterAcc += 1
+//     // } 
+
+// } else {
+//     letterAcc == 'incorrect number'
+// }
+
+let memory = {
+    cat: 'Bars',
+    dog: 'Guf'
+}
+let a = memory
+let b = memory
+console.log(a);
+console.log(b);
+console.log(memory, b);
+console.log(memory.cat);
+
+memory.cat = 'Catty'
+console.log(memory.cat);
+
+memory.bird = 'parrot'
+
+console.log(memory.bird);
+
+
+
+
+
+
+
+
+let equipment = {
+ 10:10,
+ b:111,
+ c:555
+}
+console.log(equipment);
+
+
+equipment.d = 777
+console.log(equipment);
+
+
+
+function corn () {
+console.log('hjk');
+
+}
+console.log(corn);
+
+
+
+
+function www () {
+    console.log(123);
+}
+www()
+
+const hh = function () {
+    console.log('hhhhh');
+}
+hh()
+
+
+const macesta = {
+    street1: 'Lechebnya',
+    street2: 'Lechebnya2',
+    street3: {
+        str:'Lechebnya111',
+        str2:'Lechebnya222',
+
+    },
+    macestaDictrict:function(){
+        console.log('hi');
+        
+    },
+    www
+    
+}
+console.log(macesta);
+
+let js = JSON.stringify(macesta)
+console.log(js);
+
+let antiJs = JSON.parse(js)
+console.log(antiJs);
+
+
+
+
+
+const person = {
+    name:'Dima',
+    age:30
+}
+console.log(person);
+
+
+function changeName(newName:any) {
+    newName.name+='aaa'
+    return newName
+ }
+console.log(changeName(person));
+
+
+const person3 = {...person}
+person3.name = 'Petya'
+
+console.log(person3);
+console.log(person);
+
+
+
+const person2 = person
+person2.name = 'Kolya'
+console.log(person2);
+
+
+
+function fn (a:any, b:any) {
+    a = a+100
+   return a+b
+}
+
+console.log(fn(5,8));
+console.log(fn(15,14));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const time = {
+    year: 2020,
+    mounth: 'March'
+}
+
+function changeYear (newYear:any, newMounth:any) {
+    newYear.year+=10
+    newMounth.mounth='April'
+
+    return newMounth
+}
+console.log(changeYear(time, time));
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+let aa =9 
+let bb =19
+
+if (aa>=10 && bb>=10){
+    console.log('выше допустимого значения');
+}else {
+    console.log('ниже допустимого значения');
+    
+}
+
+
+// function func () {
+//     let bb = true
+//     console.log(bb);
+// }
+
+// func()
+
+
+
+
+console.log(typeof(aa)==typeof(bb));
+
+
+// let mass = [3, 2, 5]
+// mass.forEach(el=>{
+//     setInterval (()=> {
+//         console.log(el*=3)
+//     },10000)
+
+// });
+
+
+let switchers = document.querySelector('.switcher') as HTMLDivElement
+let score = 0
+
+setInterval(()=> {
+    let x = score+=1
+    switchers.innerHTML=x
+    
+    if (switchers.innerHTML%2==0) {
+        switchers.style.backgroundColor= 'red'
+    } else {
+        switchers.style.backgroundColor= 'blue'
+    }
+    if (switchers.innerHTML>=3 && switchers.innerHTML<=10) {
+        switchers.style.backgroundColor='green'
+    }
+
+
+},10000)
+ 
+console.log(false || 'Vladimir');
+
+const battery = '500 mah'
+const battery2 = '300 mah'
+
+
+const capacity = {
+    battery,
+    battery2,
+    time
+}
+
+
+const capacity2 = `${battery} ${battery2}`
+const capacity3 = battery + ' ' + battery2
+
+console.table(capacity);
+console.log(capacity2);
+console.log(capacity3);
+
+
+
+
+// const anonim = setTimeout(() => {
+//     console.log('anonim');
+//     console.log('anonim');
+//     console.log('anonim');
+    
+// }, 500);
+
+// console.log(anonim);
+
+
+// const findError = ()=> {
+//  const findError=()=> {
+//     throw new Error('some error');
+// }
+
+//     try {
+//         findError()
+//         console.log('without mistake');
+        
+//     } catch (error) {
+//         console.log('with mistake');
+//         console.log(error);
+        
+    
+    
+// }
+
+// findError()
+
+console.log('continue...');
+
+const cat = {
+    name: 'Catty',
+    age: 3
+}
+
+const dog = {
+    name2: 'Diggy',
+    age2: 4
+}
+
+
+const animal = {
+    ...cat,
+    ...dog,
+}
+console.log(animal);
+
+
+
+console.log('hi ' + 'log');
+console.log(`${cat.name + ' ' + dog.name2}`);
+
+const myName = 'Vladimir'
+const city = 'Sochi'
+
+console.log('Меня зовут ' + myName + ' Я живу в городе ' + city);
+console.log(`Меня зовут ${myName}, я живу в городе ${city}`);
+
+
+
 
