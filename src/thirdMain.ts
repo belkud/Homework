27e0e024dc60d3bdb897f782b1f2d3d0b88a966b
belkud <vlad-belkud@yandex.ru>
2025-06-08 18:53:55 +0300

    scrollTo

--- src/thirdMain.ts
+++ src/thirdMain.ts
@@ -5,43 +5,15 @@
 
 
 let checkDiv = document.querySelector('#checkDiv') as HTMLButtonElement
-// checkDiv.style.border = '10px solid red'
-// checkDiv.style.margin = '30px'
-console.log(checkDiv.style.height);
+console.log(checkDiv.scrollTop); //! высота от родителя
+console.log(checkDiv.clientTop); // border элемента
 console.log(checkDiv.clientHeight); //! видимая внутренняя высота (сам текст)
 console.log(checkDiv.offsetHeight); //! видимая общая высота
 console.log(checkDiv.scrollHeight); //! высота вместе с невидимой частью элемента
-console.log(checkDiv.scrollTop); //! 
 console.log(checkDiv.offsetTop); //! высота от начала родителя (страницы)
-console.log(checkDiv.clientTop); // border элемента
-
-
-let accc = 0
-document.addEventListener('keydown', (e)=> {
-    if (checkDiv.offsetTop>=10) { //! ограничение передвижения элемента!!!!!!! ракета!!!!!!!!!
-        accc -=10     
-    }
-    checkDiv.style.top = accc+ 'px'
-    // checkDiv.style.scrollMarginTop = accc+ 'px'
-    
-    console.log(checkDiv.offsetTop);
-    console.log(checkDiv.scrollTop);
-    
-    console.log(getComputedStyle(checkDiv).length); // количество css-свойств
-})
-
-console.log(document.body.offsetHeight);
-console.log(document.body.clientHeight);
-console.log(document.body.scrollHeight);
-console.log(document.body.scrollTop);
-
 console.log(getComputedStyle(checkDiv).height);
 
-// if (document.body.scrollTop>=1550) {
-//     console.log('scroll');
-    
-// }
- 
+
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Размеры и прокрутка окна!!!!!!!!!!!!!!!!!
 
@@ -55,4 +27,70 @@ moveOnTop.addEventListener('click', ()=> {
         inline:'center',
     })
 })
+
+document.body.scrollHeight
+console.log(document.body.scrollHeight);
+
+
+let scrollDiv = document.querySelector('#scrollDiv') as HTMLDivElement
+let showDiv = document.querySelector('#showDiv') as HTMLDivElement
+
+// console.log(document.body.);
+
+
+document.addEventListener ('scroll', (e)=> {
+    if (window.pageYOffset + window.innerHeight>scrollDiv.offsetTop) {
+        console.log('вы дошли до элемента');
+        showDiv.style.display = 'block'
+        // alert('вы дошли до элемента')
+    }
+    // console.log(e.pageY);
+    console.log(window.pageYOffset);
+    console.log(window.scrollY);
+    console.log(window.scrollTo);
+    console.log(window.pageYOffset + window.innerHeight);
+    console.log(scrollDiv.offsetTop);
+})
+
+
+
+console.log(window.innerHeight);
+
+console.log(window.outerWidth); //! общая ширина
+console.log(document.body.clientWidth); //! ширина за вычетом полосы прокрутки и панели разработчика =)
+console.log(document.body.clientHeight);
+console.log(document.body.offsetHeight);
+console.log(document.body.scrollHeight);
+console.log(document.body.scrollTop);
+console.log(window.innerHeight);
+
+// document.body.scrollTop = '300px'
+
+
+//! window.scrollY( или window.pageYOffset) -  сколько пикселей прокручено 
+//! window.innerHeight - высота видимой области
+
+
+
+
+//! Прокрутка: scrollTo, scrollBy, scrollIntoView
+let moveOnButton = document.querySelector('#moveOnButton') as HTMLButtonElement
+moveOnButton.addEventListener('click', ()=> {
+    window.scroll(0, document.body.scrollHeight)
+    
+})
+
+
+
+let calcInput = document.querySelector('#calcInput') as HTMLInputElement
+
+
+
+// //! Прокрутка: scrollTo, scrollBy, scrollIntoView
+// moveOnButton.addEventListener('click', ()=> {
+//     calcInput.scrollTo(0, 0)
+    
+// })
+
+
 
